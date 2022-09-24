e0b93c79f011d1dd6601e32a4f7dc0a8ce2a2de1
Princengwane <hlongz920@gmail.com>
2022-09-23 18:50:57 -0700

    function that capitalizes all words of a stri
    ng

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,11 +3,12 @@
 /**
  * cap_string - capitalizes every first letter of a word in a string
  * separators of words are:  space, tabulation
- *  new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *  new line, ,, ;, ., !, ? ...
  *
  * @s: pointer to string
  * Return: pointer to s
  */
 char *cap_string(char *s)
 {
+
 }
